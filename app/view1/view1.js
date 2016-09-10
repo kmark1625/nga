ab3b20d9a462e8af7a97d469035312e85918e6ce
Kevin Mark <kmark1625@gmail.com>
2016-09-10 15:17:41 -0500

    First commit

---
+++ app/view1/view1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [function() {
+    var vm = this;
+
+    vm.platform = new H.service.Platform({
+        'app_id': 't6ldZeZcFyWw3GWaYi6G',
+        'app_code': 'ogKY0Ip_sKJro3CZe28-DA'
+    });
+
+    vm.mapTypes = vm.platform.createDefaultLayers();
+    vm.map = new H.Map(
+        document.getElementById('mapContainer'),
+        vm.mapTypes.normal.map,
+        {
+            zoom: 10,
+            center: { lat: 52.5, lng: 13.4 }
+        });
+}]);
