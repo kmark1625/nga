0e817561a51656f91a6eaaa373007f9da68b4342
Kevin Mark <kmark1625@gmail.com>
2016-09-10 22:37:27 -0500

    Add button stuff

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,12 +5,19 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl as vm'
   });
 }])
 
 .controller('View1Ctrl', ['MapService', function(MapService) {
     var vm = this;
+    vm.changeView = changeView;
 
+    vm.viewName = "Custom";
+    
     MapService.getLocation();
+
+    function changeView(view) {
+        vm.viewName = view;
+    }
 }]);
